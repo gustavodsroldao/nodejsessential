7e8fb24ed9928a36a2760ec4d932ff7f5280a121
gustavodsroldao <gustavodsroldao@gmail.com>
2025-01-30 22:44:08 -0300

    feat: node js snippets

--- modules/fs.js
+++ modules/fs.js
@@ -1,10 +1,37 @@
-const fs = require('fs')
-const path = require('path')
+newFunction()
 
-// Criar pasta
-fs.mkdir(path.join(__dirname, '/test'), (error) => {
-    if (error) {
-        return console.log('Erro', error)
-    }
-    console.log('Pasta criada com sucesso')
-})
+function newFunction() {
+    const fs = require('fs')
+    const path = require('path')
+
+    // Criar pasta
+    fs.mkdir(path.join(__dirname, '/test'), (error) => {
+        if (error) {
+            return console.log('Erro', error)
+        }
+        console.log('Pasta criada com sucesso')
+    })
+
+    // Criar e escrever em arquivo
+    fs.writeFile(path.join(__dirname, '/test', 'test.txt'), 'Hello, World!', (error) => {
+        if (error) {
+            return console.log('Erro', error)
+        }
+        console.log('Arquivo criado com sucesso')
+        // Adicionar conteudo ao arquivo
+        fs.appendFile(path.join(__dirname, '/test', 'test.txt'), 'Hello, Node!', (error) => {
+            if (error) {
+                return console.log('Erro', error)
+            }
+            console.log('Arquivo criado com sucesso')
+        }
+        )
+        // Ler arquivo
+        fs.readFile(path.join(__dirname, '/test', 'test.txt'), 'utf8', (error, data) => {
+            if (error) {
+                return console.log('Erro', error)
+            }
+            console.log(data)
+        })
+    })
+}
